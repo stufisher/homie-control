532de59a7c3c23328e2c8e696fc6c912ee41bdcf
stufisher <stu@q3f.org>
2017-05-15 11:13:53 +0200

    migrate display to generic repeater

--- devices/display.py
+++ devices/display.py
@@ -3,9 +3,9 @@
 
 import time
 import homie
-import operator as o
 import logging
 logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 from modules.homiedevice import HomieDevice
@@ -13,54 +13,31 @@ from modules.mysql import db
 
 class ListenAll(homie.Homie):
 
-    topicMap = {
-        '0268d9e0/heating/on': 'heating',
-        'control/heating/temperatureset': 'temperatureSP',
-        '028caee0/temperature/value': 'temperature',
+    topic_map = {}
+    displayid = ''
 
-        '028f39e0/temperature/value1': 'outsidetemp1',
-        '028f39e0/temperature/value2': 'outsidetemp2',
+    def __init__(self, config, db):
+        self._db = db
+        super(ListenAll, self).__init__(config)
 
-        '028f39e0/zone1/humidity': 'zone1humidity',
-        '028f39e0/zone1/pump': 'zone1pump',
-        '028f39e0/zone2/humidity': 'zone2humidity',
-        '028f39e0/zone2/pump': 'zone2pump',
-        '028f39e0/zone3/humidity': 'zone3humidity',
-        '028f39e0/zone3/pump': 'zone3pump',
-        '028f39e0/zone4/humidity': 'zone4humidity',
-        '028f39e0/zone4/pump': 'zone4pump',
-
-        '026a9de0/zone1/humidity': 'zone5humidity',
-        '026a9de0/zone1/pump': 'zone5pump',
-        '026a9de0/zone2/humidity': 'zone6humidity',
-        '026a9de0/zone2/pump': 'zone6pump',
-        '026a9de0/zone3/humidity': 'zone7humidity',
-        '026a9de0/zone3/pump': 'zone7pump',
-        '026a9de0/zone4/humidity': 'zone8humidity',
-        '026a9de0/zone4/pump': 'zone8pump',
-    }
 
     def _subscribe(self):
+        tmp = self._db.pq("""SELECT dm.round, dp.devicestring, CONCAT(p.devicestring, '/', p.nodestring, '/', p.propertystring) as propertyaddress, CONCAT(dp.devicestring, '/', dp.nodestring, '/', dp.propertystring) as repeateraddress
+            FROM repeatermap dm
+            INNER JOIN property p ON p.propertyid = dm.propertyid
+            INNER JOIN property dp ON dp.propertyid = dm.repeaterpropertyid""")
+
         logger.info('subscribing!')
-        for t in self.topicMap.keys():
-            self.mqtt.subscribe(self.baseTopic+"/"+t)
+        self.topic_map = {}
+        for t in tmp:
+            self.topic_map[t['propertyaddress']] = t
+            self.mqtt.subscribe(self.baseTopic+"/"+t['propertyaddress'])
+
+        self.displayid = t['devicestring']
         self.subscribe_all_forced = True
 
 
 class Display(HomieDevice):
-
-    _name_map = {
-        '028f39e0/zone1': 'zone1name',
-        '028f39e0/zone2': 'zone2name',
-        '028f39e0/zone3': 'zone3name',
-        '028f39e0/zone4': 'zone4name',
-        '026a9de0/zone1': 'zone5name',
-        '026a9de0/zone2': 'zone6name',
-        '026a9de0/zone3': 'zone7name',
-        '026a9de0/zone4': 'zone8name',
-    }
-
-    _displayid = 'c23e7de0'
 
     _display_online = False
 
@@ -68,26 +45,22 @@ class Display(HomieDevice):
         self._homie.mqtt.on_message = self.mqttHandler
 
     def init(self):
-        self.send_names()
-        self._homie.mqtt.subscribe('{b}/{d}/$online'.format(b=self._homie.baseTopic, d=self._displayid))
+        self.send_statics()
+        self._homie.mqtt.subscribe('{b}/{d}/$online'.format(b=self._homie.baseTopic, d=self._homie.displayid))
 
-    def send_names(self):
-        ntmp = self._db.pq("""SELECT distinct psg.name, CONCAT(p.devicestring, "/", p.nodestring) as node
-            FROM propertysubgroup psg
-            INNER JOIN propertysubgroupcomponent psgc ON psgc.propertysubgroupid = psg.propertysubgroupid
-            INNER JOIN property p ON p.propertyid = psgc.propertyid
-            WHERE p.nodestring LIKE CONCAT('zone',%s)""", ['%'])
+    def send_statics(self):
+        statics = self._db.pq("""SELECT pg.name as groupname, psg.name as subgroupname, dp.devicestring, dp.nodestring, dp.propertystring
+            FROM repeatermap dm
+            INNER JOIN property dp ON dp.propertyid = dm.repeaterpropertyid
+            LEFT OUTER JOIN propertygroup pg ON pg.propertygroupid = dm.propertygroupid
+            LEFT OUTER JOIN propertysubgroup psg ON psg.propertysubgroupid = dm.propertysubgroupid
+            WHERE psg.name IS NOT NULL OR pg.name IS NOT NULL""")
 
-        names = {}
-        for n in ntmp:
-            names[n['node']] = n['name']
+        for s in statics:
+            self._homie.mqtt.publish('{b}/{d}/{n}/{p}/set'.format(b=self._homie.baseTopic, d=s['devicestring'], n=s['nodestring'], p=s['propertystring']),
+                    payload=(s['groupname'] if s['groupname'] else s['subgroupname']))
+            time.sleep(0.5)
 
-
-        for node,p in self._name_map.iteritems():
-            if node in names:
-                self._homie.mqtt.publish('{b}/{d}/display/{p}/set'.format(b=self._homie.baseTopic, p=p, d=self._displayid),
-                    payload=str(names[node]))
-                time.sleep(0.5)
 
 
     def mqttHandler(self, client, userdata, msg, *args, **kwargs):
@@ -95,30 +68,30 @@ class Display(HomieDevice):
         if parts[0] != self._homie.baseTopic:
             return
 
-        if msg.topic == '{b}/{d}/$online'.format(b=self._homie.baseTopic, d=self._displayid):
+        if msg.topic == '{b}/{d}/$online'.format(b=self._homie.baseTopic, d=self._homie.displayid):
             if msg.payload != self._display_online:
                 self._display_online = msg.payload
 
                 if msg.payload == 'true':
                     logger.info('Display now online, sending updates')
-                    self.send_names()
+                    self.send_statics()
                     self._homie._subscribe()
                 else: 
                     logger.info('Display offline')
 
 
-        for t,nt in self._homie.topicMap.iteritems():
+        for t,nt in self._homie.topic_map.iteritems():
             if self._homie.baseTopic+"/"+t == msg.topic:
-                if t == '028caee0/temperature/value':
+                if nt['round']:
                     msg.payload = int(float(msg.payload))
-                self._homie.mqtt.publish('{b}/{d}/display/{p}/set'.format(b=self._homie.baseTopic, d=self._displayid, p=nt),
+                self._homie.mqtt.publish('{b}/{nt}/set'.format(b=self._homie.baseTopic, nt=nt['repeateraddress']),
                  payload=str(msg.payload))
                 time.sleep(0.2)
 
 
 def main():
     d = db()
-    Homie = ListenAll("configs/display.json")
+    Homie = ListenAll("configs/display.json", d)
     display = Display(d, Homie)
 
     Homie.setFirmware("display-controller", "1.0.0")
