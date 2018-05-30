b861fdef3d5a7be15eb3fe947d664e89154cb820
stufisher <stu@q3f.org>
2018-05-30 20:28:19 +0200

    add config variables + hotspot details

--- api/src/settings-sample.php
+++ api/src/settings-sample.php
@@ -16,10 +16,21 @@ return [
             'client' => 'IrrigViewer',
             'user' => '',
             'password' => '',
+
+            // mqqt server esp devices should use if different to above
+            'device_server' => '192.168.123.1',
+            'device_user'=> 'user',
+            'device_password' => 'pass',
         ],
 
         'homie' => [
             'base_topic' => 'devices',
+        ],
+
+        // Wifi AP to connect ESP devices to
+        'wifi' => [
+            'ssid' => 'myssid',
+            'password' => 'somepass',
         ],
 
         'modules' => [
@@ -31,6 +42,7 @@ return [
             'Group',
             'Property',
             'Repeater',
+            'Admin',
         ],
     ],
 ];
