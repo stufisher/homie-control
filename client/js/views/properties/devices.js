9454be9e1e9273095dce39f58aa09ff8511fa259
stufisher <stu@q3f.org>
2018-05-30 20:29:22 +0200

    add reset cell and link to scan devices

--- client/js/views/properties/devices.js
+++ client/js/views/properties/devices.js
@@ -41,6 +41,36 @@ define(['backbone.marionette', 'views/table', 'utils/table', 'utils',
         }
     })
 
+    var ResetCell = Backgrid.Cell.extend({
+        events: {
+            'click a.reset': 'reset',
+        },
+
+        reset: function(e) {
+            e.preventDefault()
+
+            if (this.resetcount > 0) {
+                this.doReset()
+            } else {
+                this.$el.append(' <span>Click again to reset</span>')
+                this.resetcount++
+                setTimeout(this.render.bind(this), 5000)
+            }
+        },
+
+        doReset: function() {
+            var message = new Paho.MQTT.Message('true')
+            message.destinationName = 'devices/'+this.model.get('devicestring')+'/$implementation/reset'
+            app.mqtt.send(message)
+        },
+
+        render: function() {
+            this.resetcount = 0
+            this.$el.html('<a href="#" class="reset"><i class="fa fa-refresh"></i></a>')
+            return this
+        }
+    })
+
 
     var AddrProperty = Property.extend({
         idAttribute: 'address',
@@ -48,7 +78,7 @@ define(['backbone.marionette', 'views/table', 'utils/table', 'utils',
     
     return Marionette.View.extend({
         className: 'content',
-        template: _.template('<div class="wrp"></div>'),
+        template: _.template('<a href="/device/scan" class="button r scan"><i class="fa fa-plus"></i> Scan for Devices</a><div class="wrp"></div>'),
         regions: {
             'wrap': '.wrp'
         },
@@ -104,6 +134,7 @@ define(['backbone.marionette', 'views/table', 'utils/table', 'utils',
                          { name: 'firmware', label: 'Firmware', cell: 'string', editable: false },
                          { name: 'version', label: 'Version', cell: 'string', editable: false },
                          { label: '', cell: TableUtils.TemplateCell, editable: false, template: '<a href="/property/s/<%=devicestring%>"><i class="fa fa-search"></i></a>' },
+                         { label: '', cell: ResetCell, editable: false },
                         ]
 
             this.table2 = new TableView({ collection: this.devices, 
