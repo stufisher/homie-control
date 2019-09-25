e601a3a6fed90d84322c1b64d1c39015b420a07b
Stu Fisher <contact@roygbiv.org.uk>
2019-09-25 07:58:42 +0200

    add radar view

---
+++ client/js/views/radar.js
@@ -0,0 +1,162 @@
+define(['backbone.marionette',
+    'leaflet',
+    'moment',
+    'models/options',
+
+    ], function(Marionette, L, moment, Options) {
+
+
+    var MapTimestamps = Backbone.Collection.extend({
+        originalSync: true,
+        url: 'https://tilecache.rainviewer.com/api/maps.json',
+
+        parse: function(r, options) {
+            var list = _.map(r, function(i) { return { timestamp: i }})
+            return list
+        },
+
+        initialize: function() {
+            this.on('change:isSelected', this.onSelectedChanged, this);
+        },
+                
+        onSelectedChanged: function(model) {
+            if (this.inUpdate) return
+
+            this.inUpdate = true
+            this.each(function(model) {
+                if (model.get('isSelected') === true && !model._changing) {
+                    model.set({isSelected: false})
+                }
+            })
+            this.inUpdate = false
+            this.trigger('selected:change', model)
+        }
+    })
+
+    return Marionette.View.extend({
+        template: _.template('<div class="label"></div><div class="map"></div>'),
+        className: 'radar',
+
+        opacity: 0.8,
+        interval: 500,
+        zoom: 8,
+        theme: 'light',
+        zoomControl: true,
+
+        ui: {
+            map: '.map',
+            label: '.label',
+        },
+
+        initialize: function(options) {
+            console.log('radar options', options)
+            this.types = {
+                osm: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                light: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+                dark: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+                radar: 'https://tilecache.rainviewer.com/v2/radar/{ts}/{size}/{z}/{x}/{y}/{color}/{options}.png'
+            }
+
+            this.timestamps = new MapTimestamps()
+            this.timestampTimeout = null
+            this.listenTo(this.timestamps, 'sync', this.renderLayers)
+            this.listenTo(this.timestamps, 'selected:change', this.setOpacity)
+
+            this.radarLayers = {}
+            this.currentFrame = 0
+
+            this.frameTimeout = null
+
+            this.options = new Options()
+            this.ready = this.options.fetch()
+
+            this.listenTo(app, 'mqtt:connect', this.getTimestamps.bind(this))
+        },
+
+        onDestroy: function() {
+            clearTimeout(this.frameTimeout)
+            clearTimeout(this.timestampTimeout)
+        },
+
+        onRender: function() {
+            this.ui.map.height(450)
+        },
+
+        getTimestamps: function() {
+            console.log('refreshing timestamps')
+            clearTimeout(this.timestampTimeout)
+
+            this.timestamps.fetch()
+            this.timestampTimeout = setTimeout(this.getTimestamps.bind(this), 1000 * 60 * 10)
+        },
+
+        nextFrame: function() {
+            clearTimeout(this.frameTimeout)
+            this.currentFrame++
+            if (this.currentFrame >= this.timestamps.length) this.currentFrame = 0
+
+            this.timestamps.at(this.currentFrame).set({ isSelected: true })
+
+            var interval = this.currentFrame == (this.timestamps.length - 1) ? this.getOption('interval')*2 : this.getOption('interval')
+            this.frameTimeout = setTimeout(this.nextFrame.bind(this), interval)
+        },
+
+        setOpacity: function() {
+            this.timestamps.each(function(ts) {
+                var t = ts.get('timestamp')
+                if (t in this.radarLayers) {
+                    this.radarLayers[t].setOpacity(ts.get('isSelected') ? this.getOption('opacity') : 0)
+                }
+            }, this)
+
+            var sel = this.timestamps.findWhere({ isSelected: true })
+            if (sel) {
+                var m = moment.unix(sel.get('timestamp'))
+                this.ui.label.text(m.format('HH:mm'))
+            }
+        },
+
+        renderLayers: function() {
+            this.timestamps.each(function(ts, i) {
+                var t = ts.get('timestamp')
+                if (!(t in this.radarLayers)) {
+                    this.radarLayers[t] = L.tileLayer(this.types.radar, {
+                        ts: t,
+                        size: 512,
+                        color: 2,
+                        options: '1_1',
+                        opactiy: 0,
+                    })
+                    this.radarLayers[t].addTo(this.map)
+                }
+            }, this)
+
+            _.each(this.radarLayers, function(layer, key) {
+                var ts = this.timestamps.findWhere({ timestamp: parseInt(key) })
+                if (!ts) this.map.removeLayer(layer)
+            }, this)
+
+            this.nextFrame()
+        },
+
+        onDomRefresh: function() {
+            this.ready.done(this.doOnDomRefresh.bind(this))
+        },
+
+        doOnDomRefresh: function() {
+            this.map = L.map(this.ui.map[0], { 
+                attributionControl: false,
+                zoomControl: this.getOption('zoomControl'),
+            }).setView([this.options.get('latitude'), this.options.get('longitude')], this.getOption('zoom'));
+
+            console.log('types', this.types, this.getOption('theme'))
+            this.base = L.tileLayer(this.types[this.getOption('theme')], {
+            }).addTo(this.map)
+
+            this.getTimestamps()
+        },
+
+    })
+
+
+})
