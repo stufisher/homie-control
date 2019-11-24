5cd888d7f5306ce78895eb3c130e6549e74dbe0f
Stu Fisher <contact@roygbiv.org.uk>
2019-11-24 17:54:14 +0100

    allow , and : in values

--- api/src/App/Modules/Property/Router.php
+++ api/src/App/Modules/Property/Router.php
@@ -37,6 +37,7 @@ class Router extends BaseRouter {
             'offset' => v::intVal(),
 
             'minimal' => v::intVal(),
+            'value' => v::alnum(',:')->noWhitespace(),
         ];
     }
 
